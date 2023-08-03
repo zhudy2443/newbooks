61998f76226f44cf08fa6657ac8ed2365a47f880
弥鸢 <1481502443@qq.com>
2023-08-04 00:56:02 +0800

    the second day achieve the login and regist function

---
+++ src/com/softeem/web/LoginServlet.java
@@ -0,0 +1,33 @@
+package com.softeem.web;
+
+import com.softeem.bean.User;
+import com.softeem.service.Impl.UserServiceImpl;
+import com.softeem.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet(name = "LoginServlet",value = "/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    private UserService userService = new UserServiceImpl();
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 1、获取请求的参数
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        // 调用 userService.login()登录处理业务
+        User loginUser = userService.login(new User(null,username,password,null));
+        // 如果等于 null,说明登录 失败!
+        if (loginUser==null){
+            // 跳回登录页面
+            req.getRequestDispatcher("/pages/user/login.html").forward(req,resp);
+        }else{
+            // 登录 成功
+            // 跳到成功页面 login_success.html
+            req.getRequestDispatcher("/pages/user/login_success.html").forward(req,resp);
+        }
+    }
+}
--- src/com/softeem/web/RegistServlet.java
+++ src/com/softeem/web/RegistServlet.java
@@ -12,17 +12,33 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @WebServlet(name = "RegistServlet",value = "/RegistServlet")
 public class RegistServlet extends HttpServlet {
+    private UserService userService = new UserServiceImpl();
+
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("utf-8");
-        String username=req.getParameter("username");
-        String password=req.getParameter("password");
-        String email=req.getParameter("email");
-        User user=new User(null,username,password,email);
-
-        UserService userService=new UserServiceImpl();
-        userService.registUser(user);
-
-        resp.sendRedirect("pages/user/regist_success.html");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 1、获取请求的参数
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+        String code = req.getParameter("code");
+        // 2、检查 验证码是否正确 === 写死,要求验证码为:abcde
+        if ("abcde".equalsIgnoreCase(code)) {
+            // 3、检查 用户名是否可用
+            if (userService.existsUsername(username)) {
+                System.out.println("用户名[" + username + "]已存在!");
+                // 跳回注册页面
+                req.getRequestDispatcher("/pages/user/regist.html").forward(req, resp);
+            } else {
+                // 可用
+                // 调用 Sservice 保存到数据库
+                userService.registUser(new User(null, username, password, email));
+                // 跳到注册成功页面regist_success.html
+                req.getRequestDispatcher("/pages/user/regist_success.html").forward(req, resp);
+            }
+        } else {
+            System.out.println("验证码[" + code + "]错误");
+            req.getRequestDispatcher("/pages/user/regist.html").forward(req, resp);
+        }
     }
 }
+
